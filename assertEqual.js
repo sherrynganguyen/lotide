f2cf4bcd8761b773a5710f8eca9c3dfa97c4f44d
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-14 23:21:10 +0000

    Add assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+let failemoji = String.fromCharCode(0x1F621);
+let passemoji = String.fromCharCode(0x1F621);
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return failemoji + " " + "Assertion Failed: " + actual + " !== " + expected;
+  } else {
+    return passemoji + " " + "Assertion Passed: " + actual + " === " + expected;
+  }
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
