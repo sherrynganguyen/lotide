81558ab2723daca0749a6d28b54a28bc5b984c06
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-15 19:11:57 +0000

    No change

--- assertEqual.js
+++ assertEqual.js
@@ -2,12 +2,14 @@ let failemoji = String.fromCharCode(0x1F621);
 let passemoji = String.fromCharCode(0x1F621);
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    return failemoji + " " + "Assertion Failed: " + actual + " !== " + expected;
+    return `${failemoji} Assertion Failed: ${actual} !== ${expected}`;
   } else {
-    return passemoji + " " + "Assertion Passed: " + actual + " === " + expected;
+    return `${passemoji} Assertion Passed: ${actual} === ${expected}`;
   }
 };
 
 // TEST CODE
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual(1, 1));
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
