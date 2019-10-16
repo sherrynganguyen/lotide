7cfecd447b1a2d668b4d8209a7689c71bce02dbd
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-16 06:25:12 +0000

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,11 +9,9 @@ const assertArraysEqual = function(array1, array2) {
   else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] === array2[i]) {
-        result = true;
         return `${failemoji} Assertion Passed: ${array1} === ${array2}`;
       }
       else {
-        result = false;
         return `${passemoji} Assertion Failed: ${array1} !== ${array2}`;
       }
     }
