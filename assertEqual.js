2184b23a2e80674d1647afaab4e5b1d2aa7cc479
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-16 02:58:47 +0000

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -11,5 +11,3 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual(1, 1));
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
