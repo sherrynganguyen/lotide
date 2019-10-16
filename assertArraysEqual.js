9b161d80608f2d44b1ee878065827761aea89a4a
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-16 16:51:59 +0000

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
 let failemoji = String.fromCharCode(0x1F621);
 let passemoji = String.fromCharCode(0x1F621);
 
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   let result = true;
   if (array1.length !== array2.length) {
     result = false;
@@ -9,14 +9,24 @@ const assertArraysEqual = function(array1, array2) {
   else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] === array2[i]) {
-        return `${failemoji} Assertion Passed: ${array1} === ${array2}`;
+        result = true;
       }
       else {
-        return `${passemoji} Assertion Failed: ${array1} !== ${array2}`;
+        result = false;
+        break;
       }
     }
   }
   return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return `${failemoji} Assertion Passed: ${array1} === ${array2}`;
+  }
+  else {
+    return `${passemoji} Assertion Failed: ${array1} !== ${array2}`;
+  }
 };
 console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
 console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
