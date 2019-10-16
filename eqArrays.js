d41d9c42a68c5ed2cd39ddf7e8df9e4d6f2b43e4
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-16 03:01:27 +0000

    Add eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -25,17 +25,14 @@ const eqArrays = function(array1, array2) {
     }
   }
   console.log(result);
-}
+};
 
 
 // TEST CODE
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
