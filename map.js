616f54873ac63d4a8126297947d553d20ffb604d
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-17 21:56:05 +0000

    Add map.js

---
+++ map.js
@@ -0,0 +1,47 @@
+
+//INSERT assertArratsEqual.js
+let failemoji = String.fromCharCode(0x1F621);
+let passemoji = String.fromCharCode(0x1F621);
+
+const eqArrays = function(array1, array2) {
+  let result = true;
+  if (array1.length !== array2.length) {
+    result = false;
+  }
+  else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        result = true;
+      }
+      else {
+        result = false;
+        break;
+      }
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return `${failemoji} Assertion Passed: ${array1} === ${array2}`;
+  }
+  else {
+    return `${passemoji} Assertion Failed: ${array1} !== ${array2}`;
+  }
+};
+
+//NEW FUNCTION - map.js
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+console.log(assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']));
