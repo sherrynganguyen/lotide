d497704431e96ffc477290e9887f60177d9d06cf
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-21 18:04:08 +0000

    Add Test files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,4 @@
+// TEST CODE
+const assertEqual = require('../assertEqual');
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+const head = require('../head.js');
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(assertEqual(head([]), 5));
