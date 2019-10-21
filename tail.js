83bdb928e484992b51be5465033308289e41c481
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-21 19:15:53 +0000

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,20 +1,9 @@
-let failemoji = String.fromCharCode(0x1F621);
-let passemoji = String.fromCharCode(0x1F621);
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    return `${failemoji} Assertion Failed: ${actual} !== ${expected}`;
-  } else {
-    return `${passemoji} Assertion Passed: ${actual} === ${expected}`;
-  }
-};
+const assertEqual = require('./assertEqual');
 
-// TEST CODE
 function tail(array) {
   let array1 = array;
   return array1.slice(1, array.length);
 }
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length, 2)); // ensure we get back two elements
-console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
-console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
-console.log(assertEqual(result, ["Lighthouse", "Labs"]));
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+module.exports = tail;
