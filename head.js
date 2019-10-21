bda9e56e4037d0c099925518abb82a9baeed4cf3
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-21 21:27:57 +0000

    Update head.js

--- head.js
+++ head.js
@@ -3,7 +3,4 @@ function head(array) {
   return array[0];
 }
 
-
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([]), 5));
+module.exports = head;
