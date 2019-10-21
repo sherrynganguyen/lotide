014e401eca014949a2c05763c9547f2bc172a1a7
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-21 19:33:52 +0000

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,4 @@
-let failemoji = String.fromCharCode(0x1F621);
-let passemoji = String.fromCharCode(0x1F621);
-
-const eqArrays = function(array1, array2) {
-  let result = true;
-  if (array1.length !== array2.length) {
-    result = false;
-  }
-  else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        result = true;
-      }
-      else {
-        result = false;
-        break;
-      }
-    }
-  }
-  return result;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -28,6 +8,6 @@ const assertArraysEqual = function(array1, array2) {
     return `${passemoji} Assertion Failed: ${array1} !== ${array2}`;
   }
 };
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
 
+
+module.export = assertArraysEqual;
